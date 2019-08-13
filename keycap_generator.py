b7f2e6a00458d4c9da21a1a85a19b37cb46e18b3
Jason Shepherd <jmanshepherd19@gmail.com>
2019-08-13 15:05:35 -0400

    added scad file output

--- keycap_generator.py
+++ keycap_generator.py
@@ -20,7 +20,7 @@ def generate_keycaps(json_path=r'keyboard-layout.json'):
         return
 
     #extracts useful key data
-    for keyboard_row in keyboard_data:
+    for ir, keyboard_row in enumerate(keyboard_data):
         default_width = 1.00
         default_height = 1.00
         skip = False
@@ -33,11 +33,15 @@ def generate_keycaps(json_path=r'keyboard-layout.json'):
                 if '\n' in key:
                     key = key.splitlines()
                 print(key, default_width, default_height)
+                create_scad_keycap(legends=key, width=default_width, height=default_height, output_path=str(ir) + " " + str(i) + r' key.scad')
             elif isinstance(key, dict):
                 if '\n' in keyboard_row[i+1]:
                     keyboard_row[i+1] = keyboard_row[i+1].splitlines()
                 print(keyboard_row[i+1], extract_metadata(key))
+                tmp_width, tmp_height = extract_metadata(key)
+                create_scad_keycap(legends=keyboard_row[i+1], width=tmp_width, height=tmp_height, output_path=str(ir) + " " + str(i) + r' key.scad')
                 skip = True
+
 
 def extract_metadata(metadata):
     "extracts metadata from dict file"
@@ -56,15 +60,27 @@ def extract_metadata(metadata):
     return width, height
 
 
-def create_scad_keycap(width=1, height=1, scad_path=r'keycap.scad'):
+def create_scad_keycap(legends, width=1, height=1, scad_path=r'keycap.scad', output_path=r'keycap_out.scad'):
     """Creates a keycap scad object from width and height"""
 
 
-    with open(scad_path, 'a+', encoding='utf-8') as scad_file:
-        scad_file.write("\ncreate_keycap(u_width=" + str(width) + ", u_height=" + str(height) + ");")
+    with open(scad_path, 'r', encoding='utf-8') as scad_file:
+        scad_keycap = scad_file.read()
         scad_file.close()
+
+    scad_keycap += "difference() {\n    create_keycap(u_width=" + str(width) + ", u_height=" + str(height) + ");\n  create_legends(["
+    if(isinstance(legends, list)):
+        for item in legends:
+            scad_keycap += r'"' + item + r'",'
+    else:
+        scad_keycap += r'"' + legends + r'"'
+
+    scad_keycap += "]);\n}"
+    with open(output_path, 'w', encoding='utf-8') as output_file:
+        output_file.write(scad_keycap)
+        output_file.close()
 
 
 if __name__ == '__main__':
-    #generate_keycaps(r'keyboard-layout.json')
-    create_scad_keycap(width=1, height=2, scad_path=r'keycap.scad')
+    generate_keycaps(r'keyboard-layout.json')
+    #create_scad_keycap(scad_path=r'keycap.scad', width=1, height=1, legends="W")
