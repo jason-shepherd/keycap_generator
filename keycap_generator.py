e433ef4b058a366e82cda0bb92b595a1af9694ca
Jason Shepherd <jmanshepherd19@gmail.com>
2019-08-15 20:46:06 -0400

    added stl creation

--- keycap_generator.py
+++ keycap_generator.py
@@ -6,7 +6,7 @@ import subprocess
 import json
 
 
-def generate_keycaps(json_path=r'keyboard-layout.json'):
+def generate_keycaps(json_path=r'keyboard-layout.json', openscad_path=r'c:\program files\openscad\openscad'):
     """generates keycap stl files from KLE RAW data"""
 
 
@@ -33,13 +33,15 @@ def generate_keycaps(json_path=r'keyboard-layout.json'):
                 if '\n' in key:
                     key = key.splitlines()
                 print(key, default_width, default_height)
-                create_scad_keycap(legends=key, width=default_width, height=default_height, output_path=str(ir) + " " + str(i) + r' key.scad')
+                create_scad_keycap(legends=raw(key), width=default_width, height=default_height, output_path=str(ir) + " " + str(i) + r' key.scad')
+                scad_to_stl(str(ir) + " " + str(i) + r' key.scad', str(ir) + " " + str(i) + r' key.stl', openscad_path)
             elif isinstance(key, dict):
                 if '\n' in keyboard_row[i+1]:
                     keyboard_row[i+1] = keyboard_row[i+1].splitlines()
                 print(keyboard_row[i+1], extract_metadata(key))
                 tmp_width, tmp_height = extract_metadata(key)
-                create_scad_keycap(legends=keyboard_row[i+1], width=tmp_width, height=tmp_height, output_path=str(ir) + " " + str(i) + r' key.scad')
+                create_scad_keycap(legends=raw(keyboard_row[i+1]), width=tmp_width, height=tmp_height, output_path=str(ir) + " " + str(i) + r' key.scad')
+                scad_to_stl(str(ir) + " " + str(i) + r' key.scad', str(ir) + " " + str(i) + r' key.stl', openscad_path)
                 skip = True
 
 
@@ -81,6 +83,30 @@ def create_scad_keycap(legends, width=1, height=1, scad_path=r'keycap.scad', out
         output_file.close()
 
 
+def scad_to_stl(scad_path, output_path, openscad_path):
+    """Use openscad command line to create stl file from scad file"""
+
+
+    openscad_process = subprocess.Popen([openscad_path, r'-o', output_path, scad_path], stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+    stdout, stderr = openscad_process.communicate()
+    print(stdout, stderr)
+
+
+def raw(text):
+    """Returns a raw string representation of text"""
+	
+	
+    escape_dict={'\a':r'\a','\b':r'\b','\c':r'\c','\f':r'\f','\n':r'\n','\r':r'\r','\t':r'\t','\v':r'\v','\'':r'\'',
+                 '\"':r'\"','\0':r'\0','\1':r'\1','\2':r'\2','\3':r'\3','\4':r'\4','\5':r'\5','\6':r'\6','\7':r'\7','\8':r'\8','\9':r'\9'}
+	
+    new_string=''
+    for char in text:
+        try: new_string+=escape_dict[char]
+        except KeyError: new_string+=char
+    return new_string
+
+
 if __name__ == '__main__':
     generate_keycaps(r'keyboard-layout.json')
+    #scad_to_stl(r'cube.scad', r'cube.stl', r'c:\program files\openscad\openscad')
     #create_scad_keycap(scad_path=r'keycap.scad', width=1, height=1, legends="W")
