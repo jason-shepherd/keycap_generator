603659021f96a95038e3c8e27620fc8d20e8f6d7
Jason Shepherd <jmanshepherd19@gmail.com>
2019-08-21 01:31:26 -0400

    refactored raw and generate_keycaps functions

--- keycap_generator.py
+++ keycap_generator.py
@@ -54,8 +54,10 @@ class KeycapGenerator:
                 if '\n' in legends:
                     legends = legends.splitlines()
                 print(legends, width, height)
-                self.create_scad_keycap(legends, width, height)
+                self.create_scad_keycap(raw(legends), width, height)
                 self.scad_to_stl(self.keycap_path, "{} {} keycap.stl".format(ir, i))
+
+        #rewrite original file data
         self.scad_file.truncate(0)
         self.scad_file.seek(0)
         self.scad_file.write(self.scad_keycap)
@@ -96,20 +98,21 @@ class KeycapGenerator:
 def raw(text):
     """Returns a raw string representation of text"""
 
+
+    new_text = None
     escape_dict = {'\a': r'\a', '\b': r'\b', '\c': r'\c', '\f': r'\f', '\n': r'\n', '\r': r'\r',
                    '\t': r'\t', '\v': r'\v', '\'': r'\'', '\"': r'\"', '\0': r'\0', '\1': r'\1',
                    '\2': r'\2', '\3': r'\3', '\4': r'\4', '\5': r'\5', '\6': r'\6', '\7': r'\7',
                    '\8': r'\8', '\9': r'\9'}
+
     if isinstance(text, str):
-        new_string = ''
         for char in text:
             try:
-                new_string += escape_dict[char]
+                new_text += escape_dict[char]
             except KeyError:
-                new_string += char
-        return new_string
-    elif isinstance(text, list):
-        new_list = []
+                new_text += char
+    else:
+        new_text = []
         for item in text:
             for char in item:
                 new_string = ''
@@ -117,8 +120,8 @@ def raw(text):
                     new_string += escape_dict[char]
                 except KeyError:
                     new_string += char
-                new_list.append(new_string)
-            return new_list
+                new_text.append(new_string)
+    return new_text
 
 
 if __name__ == '__main__':
