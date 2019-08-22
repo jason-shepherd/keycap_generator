bea9a5905c2afc4ceeb7cdd87d7b004fc125c49a
Jason Shepherd <jmanshepherd19@gmail.com>
2019-08-21 22:43:33 -0400

    fixed string parsing error

--- keycap_generator.py
+++ keycap_generator.py
@@ -103,15 +103,16 @@ def raw(text):
     escape_dict = {'\a': r'\a', '\b': r'\b', '\c': r'\c', '\f': r'\f', '\n': r'\n', '\r': r'\r',
                    '\t': r'\t', '\v': r'\v', '\'': r'\'', '\"': r'\"', '\0': r'\0', '\1': r'\1',
                    '\2': r'\2', '\3': r'\3', '\4': r'\4', '\5': r'\5', '\6': r'\6', '\7': r'\7',
-                   '\8': r'\8', '\9': r'\9'}
+                   '\8': r'\8', '\9': r'\9', '\\' : r'\\\\'}
 
     if isinstance(text, str):
+        new_text = ''
         for char in text:
             try:
                 new_text += escape_dict[char]
             except KeyError:
                 new_text += char
-    else:
+    elif isinstance(text, list):
         new_text = []
         for item in text:
             for char in item:
